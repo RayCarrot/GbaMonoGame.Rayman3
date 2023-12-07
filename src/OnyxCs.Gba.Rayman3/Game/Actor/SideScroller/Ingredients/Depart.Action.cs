c1f8f9cf33e05fee8af7b78768b59280af3fefab
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-12-07 21:45:24 +0100

    Add enum for Depart actor actions

---
+++ src/OnyxCs.Gba.Rayman3/Game/Actor/SideScroller/Ingredients/Depart.Action.cs
@@ -0,0 +1,16 @@
+﻿namespace OnyxCs.Gba.Rayman3;
+
+public partial class Depart
+{
+    private new Action ActionId
+    {
+        get => (Action)base.ActionId;
+        set => base.ActionId = (int)value;
+    }
+
+    private enum Action
+    {
+        ExitLevel = 0,
+        EndLevel = 1,
+    }
+}
