9a14c61c7e28c417018bd6cc97c269ec3d77a63f
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-01-14 18:06:01 +0100

    Show action table in debug menu

--- src/GbaMonoGame.Engine2d/DebugWindows/GameObjectDebugWindow.cs
+++ src/GbaMonoGame.Engine2d/DebugWindows/GameObjectDebugWindow.cs
@@ -1,4 +1,5 @@
 ﻿using ImGuiNET;
+using Action = BinarySerializer.Ubisoft.GbaEngine.Action;
 
 namespace GbaMonoGame.Engine2d;
 
@@ -21,6 +22,48 @@ public class GameObjectDebugWindow : DebugWindow
                 selectedGameObject.Position = new Vector2(pos.X, pos.Y);
 
             selectedGameObject.DrawDebugLayout(debugLayout, textureManager);
+
+            if (selectedGameObject is ActionActor actionActor)
+            {
+                ImGui.Spacing();
+                ImGui.Spacing();
+                ImGui.SeparatorText("Actions");
+
+                if (ImGui.BeginTable("_actions", 5))
+                {
+                    ImGui.TableSetupColumn("Current", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("Id", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("Animation", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("Flags", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("MechModel");
+                    ImGui.TableHeadersRow();
+
+                    for (int actionId = 0; actionId < actionActor.Actions.Length; actionId++)
+                    {
+                        Action action = actionActor.Actions[actionId];
+                        ImGui.TableNextRow();
+
+                        ImGui.TableNextColumn();
+                        bool isCurrent = actionActor.ActionId == actionId;
+                        if (ImGui.RadioButton($"##{actionId}_enabled", isCurrent))
+                            actionActor.ActionId = actionId;
+
+                        ImGui.TableNextColumn();
+                        ImGui.Text($"{actionId}");
+
+                        ImGui.TableNextColumn();
+                        ImGui.Text($"{action.AnimationIndex}");
+
+                        ImGui.TableNextColumn();
+                        ImGui.Text($"{action.Flags}");
+
+                        ImGui.TableNextColumn();
+                        ImGui.Text($"{action.MechModelType}");
+                    }
+
+                    ImGui.EndTable();
+                }
+            }
         }
         else
         {
