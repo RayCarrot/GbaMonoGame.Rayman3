c35a41ffd40b6ac18ef2f9e928ba16fa50b2c700
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-01-15 12:44:00 +0100

    Update depart actor comment

--- src/GbaMonoGame.Rayman3/Game/Actor/SideScroller/Ingredients/Depart.cs
+++ src/GbaMonoGame.Rayman3/Game/Actor/SideScroller/Ingredients/Depart.cs
@@ -6,7 +6,7 @@ public sealed partial class Depart : ActionActor
 {
     public Depart(int instanceId, Scene2D scene, ActorResource actorResource) : base(instanceId, scene, actorResource)
     {
-        // The behavior to end a level with the sign facing to the right seems unused in the game
+        // The behavior to end a level with the sign facing to the right is unused in the final version of the game
         MessageToSend = (Action)actorResource.FirstActionId == Action.EndLevel ? Message.Main_LevelEnd : Message.Main_LevelExit;
         Fsm.ChangeAction(Fsm_Idle);
     }
