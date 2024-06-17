d2a41a06bb2ab6736c3df772c18331c83feb72c8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-06-17 08:26:32 +0200

    Read 3D data in credits

--- src/GbaMonoGame.Rayman3/Game/Credits.cs
+++ src/GbaMonoGame.Rayman3/Game/Credits.cs
@@ -1,9 +1,17 @@
-﻿namespace GbaMonoGame.Rayman3;
+﻿using BinarySerializer.Ubisoft.GbaEngine;
+
+namespace GbaMonoGame.Rayman3;
 
 // TODO: Implement
 // TODO: Add to frames selection
 public class Credits : Frame
 {
+    public override void Init()
+    {
+        Storage.LoadResource<AnimActor>(126);
+        Storage.LoadResource<TextureTable>(127);
+    }
+
     public override void Step()
     {
         
