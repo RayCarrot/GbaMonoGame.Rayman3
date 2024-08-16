bb17b8a72340f0870da2268162d20aaf6061ca8a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-08-16 21:26:36 +0200

    Fix Woods of Light background size on N-Gage

--- src/GbaMonoGame.Rayman3/Game/Level/World_1/WoodLight_M1.cs
+++ src/GbaMonoGame.Rayman3/Game/Level/World_1/WoodLight_M1.cs
@@ -14,13 +14,19 @@ public class WoodLight_M1 : FrameSideScroller
         Scene.AddDialog(new TextBoxDialog(Scene), false, false);
 
         // TODO: Add config option for scrolling on N-Gage
+        TgxTileLayer cloudsLayer = ((TgxPlayfield2D)Scene.Playfield).TileLayers[0];
+        TextureScreenRenderer renderer = (TextureScreenRenderer)cloudsLayer.Screen.Renderer;
         if (Engine.Settings.Platform == Platform.GBA)
         {
-            TgxTileLayer cloudsLayer = ((TgxPlayfield2D)Scene.Playfield).TileLayers[0];
-            cloudsLayer.Screen.Renderer = new LevelCloudsRenderer(((TextureScreenRenderer)cloudsLayer.Screen.Renderer).Texture, new[]
+            cloudsLayer.Screen.Renderer = new LevelCloudsRenderer(renderer.Texture, new[]
             {
                 32, 120, 227
             });
+        }
+        else
+        {
+            // Need to limit the background to 256 since the rest is just transparent
+            renderer.TextureRectangle = renderer.TextureRectangle with { Width = 256 };
         }
     }
 }
--- src/GbaMonoGame.TgxEngine/Renderer/TextureScreenRenderer.cs
+++ src/GbaMonoGame.TgxEngine/Renderer/TextureScreenRenderer.cs
@@ -8,13 +8,15 @@ public class TextureScreenRenderer : IScreenRenderer
     public TextureScreenRenderer(Texture2D texture)
     {
         Texture = texture;
+        TextureRectangle = texture.Bounds;
     }
 
     public Texture2D Texture { get; }
+    public Rectangle TextureRectangle { get; set; }
 
-    public Vector2 GetSize(GfxScreen screen) => new(Texture.Width, Texture.Height);
+    public Vector2 GetSize(GfxScreen screen) => TextureRectangle.Size.ToVector2();
     public void Draw(GfxRenderer renderer, GfxScreen screen, Vector2 position, Color color)
     {
-        renderer.Draw(Texture, position, color);
+        renderer.Draw(Texture, position, TextureRectangle, color);
     }
 }
