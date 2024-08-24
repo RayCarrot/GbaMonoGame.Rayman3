34e97ba8b24f024aac83a8c0ab95deb134aa5753
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-08-24 19:52:10 +0200

    Implement palette glowing for switch bar

--- src/GbaMonoGame.Rayman3/Game/Dialog/Bars/SwitchBar.cs
+++ src/GbaMonoGame.Rayman3/Game/Dialog/Bars/SwitchBar.cs
@@ -1,4 +1,6 @@
-﻿using BinarySerializer.Ubisoft.GbaEngine;
+﻿using System;
+using BinarySerializer;
+using BinarySerializer.Ubisoft.GbaEngine;
 using GbaMonoGame.AnimEngine;
 
 namespace GbaMonoGame.Rayman3;
@@ -36,6 +38,28 @@ public class SwitchBar : Bar
             Camera = Scene.HudCamera,
         };
 
+        // The original game dynamically modifies the loaded palette in the Draw method to make
+        // the switches appear like they're glowing. The easiest way to replicate it here is to
+        // create separate palettes that we cycle between.
+        if (Switches.Resource.PalettesCount == 1)
+        {
+            PaletteResource originalPalette = Switches.Resource.Palettes.Palettes[0];
+
+            Switches.Resource.PalettesCount = 9;
+            Switches.Resource.Palettes.Palettes = new PaletteResource[Switches.Resource.PalettesCount];
+
+            for (int i = 0; i < Switches.Resource.Palettes.Palettes.Length; i++)
+            {
+                RGB555Color[] colors = new RGB555Color[originalPalette.Colors.Length];
+                Array.Copy(originalPalette.Colors, colors, originalPalette.Colors.Length);
+
+                colors[2] = new RGB555Color((uint)((i + 23) * 0x20));
+                colors[9] = new RGB555Color((uint)((i + 4) * 0x400 | 0x160 | i + 23));
+
+                Switches.Resource.Palettes.Palettes[i] = new PaletteResource { Colors = colors };
+            }
+        }
+
         PaletteShiftValue = 0;
         PaletteShiftDirection = 0;
     }
@@ -45,12 +69,23 @@ public class SwitchBar : Bar
     public override void Draw(AnimationPlayer animationPlayer)
     {
         if (((FrameSideScroller)Frame.Current).CurrentStepAction == ((FrameSideScroller)Frame.Current).Step_Normal)
+            Switches.BasePaletteIndex = PaletteShiftValue;
+
+        if ((GameTime.ElapsedFrames & 3) == 3)
         {
-            // TODO: Update palette
+            if (PaletteShiftDirection == 0)
+            {
+                PaletteShiftValue++;
+                if (PaletteShiftValue == 8)
+                    PaletteShiftDirection = 1;
+            }
+            else
+            {
+                PaletteShiftValue--;
+                if (PaletteShiftValue == 0)
+                    PaletteShiftDirection = 0;
+            }
         }
-
-        // TODO: Update palette value
-
         if (Mode is BarMode.StayHidden or BarMode.Disabled)
             return;
 
