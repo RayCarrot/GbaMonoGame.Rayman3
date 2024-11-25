f441813b97320bdc3740b92ce036a5da69a57d29
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-11-25 19:36:29 +0100

    Circle wipe fx fixes

--- src/GbaMonoGame.Rayman3/Game/Effect/CircleWindowWipeEffectObject.cs
+++ src/GbaMonoGame.Rayman3/Game/Effect/CircleWindowWipeEffectObject.cs
@@ -24,18 +24,21 @@ public class CircleWindowWipeEffectObject : EffectObject
         // Bottom-right
         if (Value < quarterCircle * 1)
         {
-            float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 1 - Value);
-            float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 1 - Value);
-            float triangleLength = horizontalScale / verticalScale;
-
-            for (float y = halfRes.Y; y < res.Y; y++)
+            if (Value != quarterCircle * 0)
             {
-                float length = (y - halfRes.Y) * triangleLength;
+                float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 1 - Value);
+                float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 1 - Value);
+                float triangleLength = horizontalScale / verticalScale;
 
-                if (length > halfRes.X)
-                    length = halfRes.X;
+                for (float y = halfRes.Y; y < res.Y; y++)
+                {
+                    float length = (y - halfRes.Y) * triangleLength;
 
-                DrawLine(new Vector2(halfRes.X, y), new Vector2(halfRes.X + length, y), Color.Black);
+                    if (length > halfRes.X)
+                        length = halfRes.X;
+
+                    DrawLine(new Vector2(halfRes.X, y), new Vector2(halfRes.X + length, y), Color.Black);
+                }
             }
         }
         // Top-right
@@ -44,38 +47,44 @@ public class CircleWindowWipeEffectObject : EffectObject
             // Fill bottom-right
             DrawRectangle(halfRes, halfRes, Color.Black);
 
-            float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 1 - Value);
-            float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 3 - Value);
-            float triangleLength = horizontalScale / verticalScale;
-
-            for (int y = 0; y < halfRes.Y; y++)
+            if (Value != quarterCircle * 1)
             {
-                float length = (halfRes.Y - y) * triangleLength;
+                float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 1 - Value);
+                float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 3 - Value);
+                float triangleLength = horizontalScale / verticalScale;
 
-                if (length > halfRes.X)
-                    length = halfRes.X;
+                for (int y = 0; y < halfRes.Y; y++)
+                {
+                    float length = (halfRes.Y - y) * triangleLength;
 
-                DrawLine(new Vector2(halfRes.X + length, y), new Vector2(res.X, y), Color.Black);
+                    if (length > halfRes.X)
+                        length = halfRes.X;
+
+                    DrawLine(new Vector2(halfRes.X + length, y), new Vector2(res.X, y), Color.Black);
+                }
             }
         }
         // Top-left
         else if (Value < quarterCircle * 3)
         {
-            // Fill bottom-right
-            DrawRectangle(halfRes, halfRes, Color.Black);
+            // Fill right
+            DrawRectangle(new Vector2(halfRes.X, 0), new Vector2(halfRes.X, res.Y), Color.Black);
 
-            float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 3 - Value);
-            float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 3 - Value);
-            float triangleLength = horizontalScale / verticalScale;
-
-            for (int y = 0; y < halfRes.Y; y++)
+            if (Value != quarterCircle * 2)
             {
-                float length = (halfRes.Y - y) * triangleLength;
+                float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 3 - Value);
+                float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 3 - Value);
+                float triangleLength = horizontalScale / verticalScale;
 
-                if (length > halfRes.X)
-                    length = halfRes.X;
+                for (int y = 0; y < halfRes.Y; y++)
+                {
+                    float length = (halfRes.Y - y) * triangleLength;
 
-                DrawLine(new Vector2(halfRes.X - length, y), new Vector2(res.X, y), Color.Black);
+                    if (length > halfRes.X)
+                        length = halfRes.X;
+
+                    DrawLine(new Vector2(halfRes.X - length, y), new Vector2(halfRes.X, y), Color.Black);
+                }
             }
         }
         // Bottom-left
@@ -87,18 +96,21 @@ public class CircleWindowWipeEffectObject : EffectObject
             // Fill bottom-right
             DrawRectangle(halfRes, halfRes, Color.Black);
 
-            float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 3 - Value);
-            float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 5 - Value);
-            float triangleLength = horizontalScale / verticalScale;
-
-            for (float y = halfRes.Y; y < res.Y; y++)
+            if (Value != quarterCircle * 3)
             {
-                float length = (y - halfRes.Y) * triangleLength;
+                float horizontalScale = scalingFactor * MathHelpers.Cos256(quarterCircle * 3 - Value);
+                float verticalScale = scalingFactor * MathHelpers.Sin256(quarterCircle * 5 - Value);
+                float triangleLength = horizontalScale / verticalScale;
 
-                if (length > halfRes.X)
-                    length = halfRes.X;
+                for (float y = halfRes.Y; y < res.Y; y++)
+                {
+                    float length = (y - halfRes.Y) * triangleLength;
 
-                DrawLine(new Vector2(0, y), new Vector2(halfRes.X - length, y), Color.Black);
+                    if (length > halfRes.X)
+                        length = halfRes.X;
+
+                    DrawLine(new Vector2(0, y), new Vector2(halfRes.X - length, y), Color.Black);
+                }
             }
         }
     }
